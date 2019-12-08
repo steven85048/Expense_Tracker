b1d1d3a354fd4b3e664952c36af677273b1c0583
Steven Tran <steven85048@gmail.com>
2019-12-07 18:00:46 -0700

    Add ecr permissions to networks-messaging-role

--- infrastructure_config/aws/hostroles/networks-messaging-server.tf
+++ infrastructure_config/aws/hostroles/networks-messaging-server.tf
@@ -12,6 +12,14 @@ resource "aws_iam_role" "networks-messaging-server-assume-role" {
             },
             "Effect": "Allow",
             "Sid": ""
+        },
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
         }
     ]
 }
@@ -21,6 +29,36 @@ EOF
 
 resource "aws_iam_instance_profile" "networks-messaging-server-profile" {
   name = "networks-messaging-server-profile"
-  role = aws_iam_role.transaction-service-assume-role.name
+  role = aws_iam_role.networks-messaging-server-assume-role.name
 }
 
+resource "aws_iam_role_policy" "networks-messaging-server-policy" {
+  name   = "networks-messaging-server-policy"
+  role   = aws_iam_role.networks-messaging-server-assume-role.id
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "GetAndListRepository",
+            "Effect": "Allow",
+            "Action": [
+                "ecr:Describe*",
+                "ecr:Get*",
+                "ecr:List*",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability"
+            ],
+            "Resource": "arn:aws:ecr:us-east-2:003195358776:repository/expense_tracker_ecr"
+        },
+        {
+            "Sid": "GetAuthorization",
+            "Effect": "Allow",
+            "Action": "ecr:GetAuthorizationToken",
+            "Resource": "*"
+        }
+    ]
+}   
+EOF
+
+}
